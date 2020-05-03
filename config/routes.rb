247e0f13214a527ebdcf8ef2eafc5fd77085de36
vagrant <vagrant@localhost.localdomain>
2020-05-03 08:35:10 +0000

    [advance4]finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resource :book_comments, only: [:create, :destroy]
+  end
+  resources :relationships, only: [:create, :destroy]
+
+  resources :users,only: [:show,:index,:edit,:update] do
+  	member do
+      get :following, :followers
+    end
   end
 end
